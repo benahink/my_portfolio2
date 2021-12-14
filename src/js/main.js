19da21ce458a3473ee21072b7fe43a12682ce141
Benahin Kur <benahink@gmail.com>
2021-12-13 21:38:13 -0700

    mvp

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+const navLink = document.querySelectorAll(".nav_link");
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+  return "linkAction fired";
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
